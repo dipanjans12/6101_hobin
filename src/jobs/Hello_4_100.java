2237742968f2df00f05978f136d959c6cb77e1b4
Hobin Yoon <hobinyoon@gmail.com>
2014-04-04 10:01:50 -0400

    more jobs to test fairness

---
+++ src/jobs/Hello_4_100.java
@@ -0,0 +1,25 @@
+package jobs;
+
+import java.text.SimpleDateFormat;
+import common.*;
+
+//Hello World
+public class Hello_4_100 extends Job {
+  static final SimpleDateFormat _sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+
+  @Override
+  public void config() {
+    setNumTasks(4); //set the number of tasks
+  }
+
+  @Override
+  public void task(int tId) {
+    System.out.println(_sdf.format(System.currentTimeMillis()) +
+        " task"+tId+": Hello_4_1000"); //this string will be printed out from worker instead of client
+    try{
+      Thread.sleep(100);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
