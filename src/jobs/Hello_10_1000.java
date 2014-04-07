3cbd37bb1ff211b5602e9dbf391d55f23ac6c655
Hobin Yoon <hobinyoon@gmail.com>
2014-04-07 00:34:25 -0400

    more jobs

---
+++ src/jobs/Hello_10_1000.java
@@ -0,0 +1,25 @@
+package jobs;
+
+import java.text.SimpleDateFormat;
+import common.*;
+
+//Hello World
+public class Hello_10_1000 extends Job {
+  static final SimpleDateFormat _sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+
+  @Override
+  public void config() {
+    setNumTasks(10); //set the number of tasks
+  }
+
+  @Override
+  public void task(int tId) {
+    //System.out.println(_sdf.format(System.currentTimeMillis()) +
+    //    " task"+tId+": Hello_2_1000"); //this string will be printed out from worker instead of client
+    try{
+      Thread.sleep(1000);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
